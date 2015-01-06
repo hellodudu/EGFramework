679454b017b9b8eae94d61ece8ccce708f2e44fc
Anthony Jiang <2nth0nyj@gmail.com>
2015-01-06 16:25:04 +0800

    Add configuration file translator, put database configuration in config file.

--- src/connector/account.erl
+++ src/connector/account.erl
@@ -14,13 +14,13 @@ handle({CsAccountLogin, Session}) when erlang:is_record(CsAccountLogin, cs_accou
     ResponseRecord = 
         case riakc_pb_socket:get(RiakConnectionPid, {<<"player">>,<<"account">>},Key) of
             {ok, AccountObject} ->
-                start_role_process(),
+                start_role_process(Session),
                 #sc_account_login{ result = ?SUCCESS };
             {error, _ } ->
                 #sc_account_login{ result = ?ROLE_NOT_EXISTED }
         end,
     connector:send_to_role( ConnectorPid, ResponseRecord ),
-    NewSession = Session#session{account_id=AccountId,riak_connection_pid=RiakConnectionPid },
+    NewSession = Session#session{account_id=AccountId,riak_connection_pid=RiakConnectionPid},
     {ok, NewSession};
 
 handle({CsAccountCreateRole,Session}) when erlang:is_record(CsAccountCreateRole, cs_account_create_role) ->
@@ -35,17 +35,17 @@ handle({CsAccountCreateRole,Session}) when erlang:is_record(CsAccountCreateRole,
     AccountKey = erlang:integer_to_binary(AccountId),
     AccountObject = riakc_obj:new({<<"player">>,<<"account">>}, AccountKey, erlang:term_to_binary(AccountRecord) ),
     ResponseRecord = #sc_account_create_role{result=?SUCCESS, role_id=RoleKey},
-    {ok, RolePid} = start_role_process(),
+    {ok, RolePid} = start_role_process(Session),
     connector:send_to_role(ConnectorPid, ResponseRecord),
-    NewSession = #session{role_pid=RolePid, role_id = RoleKey },
+    NewSession = #session{role_pid=RolePid, role_id = RoleKey},
     {ok, NewSession};
 
 establish_riak_connection() ->
-    {ok, RiakNodeList} = application:get_env(connector, riak_node_list),
-    RandomNode = lists:nth( random:uniform( erlang:length(RiakNodeList) ), RiakNodeList ),
+    {ok, RiakNodeList} = server_config:get(riak_node_list),
+    RandomNode = lists:nth(random:uniform(erlang:length(RiakNodeList)),RiakNodeList),
     {Ip, Port} = RandomNode,
     {ok, RiakConnectionPid } = riakc_pb_socket:start_link(Ip,Port),
     {ok, RiakConnectionPid }.
 
-start_role_process() ->
+start_role_process(Session) ->
     ok.
